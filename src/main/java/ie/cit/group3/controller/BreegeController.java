a664605a739bcc6ca04663dc7ca1d2803d8ed41a
bmoyLK <bridget.moynihan@mycit.ie>
2015-04-30 14:15:46 +0100

    BM

--- src/main/java/ie/cit/group3/controller/BreegeController.java
+++ src/main/java/ie/cit/group3/controller/BreegeController.java
@@ -2,15 +2,10 @@ package ie.cit.group3.controller;
 
 import java.util.List;
 
-import ie.cit.group3.JdbcTemplateTestApplication;
 import ie.cit.group3.domain.ChObject;
-import ie.cit.group3.domain.Image;
-import ie.cit.group3.domain.Participant;
-import ie.cit.group3.domain.Role;
+
 import ie.cit.group3.service.ChObjectService;
 import ie.cit.group3.service.ImageService;
-import ie.cit.group3.service.ParticipantService;
-import ie.cit.group3.service.RolesService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -19,60 +14,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/browse")
+@RequestMapping("/display")
 public class BreegeController {
 	
 	@Autowired
 	ChObjectService chobject;
 	
 	@Autowired
-	RolesService roles;
-	
-	@Autowired
-	ParticipantService participants;
-	
-	@Autowired
 	ImageService images;
 	
-	@RequestMapping(value="/listall/objects", method = RequestMethod.GET) 
-	public String listAll(ModelMap model) {			
-			
-			List<ChObject> listobjects =chobject.findAll();
-			model.addAttribute("objects", listobjects);
-		    return "displayObjects";			
-		}  
-//	
-//	@RequestMapping(value="/listall/import", method = RequestMethod.GET) 
-//	public String importObjects(ModelMap model) {			
-//			
-//		JdbcTemplateTestApplication importObjects = new JdbcTemplateTestApplication();
-//			
-//		List<ChObject> listobjects =chobject.findAll();
-//			model.addAttribute("objects", listobjects);
-//		    return "displayObjects";			
-//		}   
-	
-	@RequestMapping(value="/listall/roles", method = RequestMethod.GET) 
+	@RequestMapping(value="/listall/chobject", method = RequestMethod.GET) 
 	public String listRole(ModelMap model) {			
-			
-			List<Role> listroles = roles.findAll();
-			model.addAttribute("roles", listroles);
-		    return "displayRoles";			
+			ChObject listchobject = new ChObject();
+			listchobject.setId("1");
+			listchobject.setTitle("a title");					
+			//List<ChObject> listchobject = chobject.findAll();
+			model.addAttribute("chobject", listchobject);
+		    return "displayObject";			
 		}    
 	
-	@RequestMapping(value="/listall/participants", method = RequestMethod.GET) 
-	public String listParticipant(ModelMap model) {			
-			
-			List<Participant> listparticipants = participants.findAll();
-			model.addAttribute("participants", listparticipants);
-		    return "displayParticipants";			
-		}    
-	
-	@RequestMapping(value="/listall/images", method = RequestMethod.GET) 
-	public String listImage(ModelMap model) {			
-			
-			List<Image> listimages = images.findAll();
-			model.addAttribute("images", listimages);
-		    return "displayImages";			
-		}  
 }             
